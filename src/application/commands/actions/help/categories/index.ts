8304bcd733de67283ad8929970b8b0bfddf8f4ed
felipejhordan <felipejordan.alves@gmail.com>
2022-04-24 00:28:25 -0300

    feat: create status code constant

---
+++ src/application/commands/actions/help/categories/index.ts
@@ -0,0 +1,8 @@
+import helpHelpCategory from "./help.help.category";
+import jokesHelpCategory from "./jokes.help.category";
+
+
+export const allCategories = {
+    jokes: jokesHelpCategory.helpTexts,
+    help: helpHelpCategory.helpTexts
+}
---
+++ src/application/config/constants/statusCode.ts
@@ -0,0 +1,9 @@
+ const INTERNAL_ERROR = 500
+ const SERVICE_UNAVAILABLE = 500
+ const OK = 200
+
+ export const statusCode = {
+     INTERNAL_ERROR,
+     SERVICE_UNAVAILABLE,
+     OK
+ }
