a2b840ef3a75e8b56be439a5952efd2e04a6e17c
felipejhordan <felipejordan.alves@gmail.com>
2022-04-24 00:28:43 -0300

    refactor: add new imp in command.validator

--- src/application/validators/command.validator.ts
+++ src/application/validators/command.validator.ts
@@ -12,7 +12,8 @@ export const commandValidator = (message: Message): commandValidatorResult|void 
     if(!message.content.toLowerCase().startsWith(PREFIX.toLowerCase())) return;
 
     const inputWithoutPreffix = message.content.trim().slice(PREFIX.length)
-    const [command, args ] = inputWithoutPreffix.split(BLANK_SPACE, 2)
+    const command = inputWithoutPreffix.split(BLANK_SPACE, 2)[0].toLowerCase()
+    const args = inputWithoutPreffix.replace(command, '').split(" ").filter(s => s != '')
     return {
         args,
         command
