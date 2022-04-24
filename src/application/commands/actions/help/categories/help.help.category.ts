a3b2f3eef5a1552f04539d8740127e335bdf2dc5
felipejhordan <felipejordan.alves@gmail.com>
2022-04-24 00:27:09 -0300

    feat: create help list category

---
+++ src/application/commands/actions/help/categories/help.help.category.ts
@@ -0,0 +1,10 @@
+
+export default  {
+    helpTexts: [{
+        command: "help",
+        description: "todos comandos!"
+    }, {
+        command: "help <categoria>",
+        description: "comandos de uma categoria"
+    },]
+}
---
+++ src/application/commands/actions/help/categories/jokes.help.category.ts
@@ -0,0 +1,14 @@
+
+export default  {
+    helpTexts: [{
+        command: "send-hello",
+        description: "Dando um hello"
+    }, {
+        command: "fish",
+        description: "Aquela boa e velha piada do bol-cat"
+    },
+    {
+        command: "jokes-chuck",
+        description: "piadas do chuck norris"
+    }]
+}
