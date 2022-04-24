db470770181c2ef463c93350f8d04bd9947a91ae
felipejhordan <felipejordan.alves@gmail.com>
2022-04-24 00:29:37 -0300

    feat: create main file with init and listener callbacks

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
-import 'dotenv/config'
-console.log("Vou iniciar meu boot")
+import { Client, Intents } from "discord.js";
+import "dotenv/config";
+import { Message } from "../node_modules/discord.js/typings/index";
+import { discordConfig } from "./application/config/discord.config";
+import { messageListener } from "./application/listeners/message/message.listener";
 
-console.log(process.env.DISCORD_TOKEN)
+const init = async () => {
+  try {
+    const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]});
+
+    client.once("ready", () => {
+      console.log("Ready!");
+    });
+    
+     client.on('messageCreate',async (message: Message) =>  await messageListener({client, message}));
+     client.login(discordConfig.discordToken)
+
+    
+      
+  } catch (e) {
+      console.error("App Init error!");
+      console.error(JSON.stringify(e));
+  }
+};
+
+init()
